520148e16cb91b6a75ea3daca96063974565e93d
samarth-moschip <samarth.sambrekar@moschip.com>
2025-04-02 17:35:14 +0530

    Delete python_intermediate directory

--- python_intermediate/cube_list.py
+++
@@ -1,4 +0,0 @@
-list1=[1,2,3,4,5]
-
-for i in range(0,len(list1)):
-    print(list1[i]**3)
--- python_intermediate/structs.py
+++
@@ -1,6 +0,0 @@
-
-import struct
-
-converted_bytes = struct.pack('3i',1,2,3)
-
-print(struct.unpack('3i', converted_bytes))
