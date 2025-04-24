2005b51b796707724a5a305155be7dfe9ec8f32b
samarth-moschip <samarth.sambrekar@moschip.com>
2025-04-24 12:29:50 +0530

    new updated
    Signed-off-by: samarth-moschip <samarth.sambrekar@moschip.com>

--- python_basic_program/evenodd.py
+++
@@ -1,9 +0,0 @@
-# Problem Statement:
-#This script display given number is odd or even
-
-num=int(input("enter number:"))
-
-if num%2==0:
-    print("given number is even")
-else:
-    print("given number is odd")    
--- python_basic_program/excp.py
+++
@@ -1,3 +0,0 @@
-# Problem Statement:
-# This script display all exception in python
-print(dir(locals()['__builtins__']))
